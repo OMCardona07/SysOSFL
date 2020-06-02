fe43f60a9a4144cbe9f7ddeaf84ff172b84c07e1
Josué Cardona <josuecardona07@hotmail.com>
2020-06-01 21:27:46 -0600

    Agregacion de metodos

--- SysOSFL.BL/AdministradorBL.cs
+++ SysOSFL.BL/AdministradorBL.cs
@@ -11,9 +11,42 @@ namespace SysOSFL.BL
     public class AdministradorBL
     {
         AdministradorDAL DAL = new AdministradorDAL();
-        public int AgregarAdministrador(Administrador pAdmin)
+        public int AgregarAdmin(Administrador pAdmin)
         {
             return DAL.AgregarAdmin(pAdmin);
+        }
+
+        public int ModificarAdministracion(Administrador pAdminin)
+        {
+            return AdministradorDAL.ModificarAdministrador(pAdminin);
+        }
+
+        public List<Administrador> ObtenerTodosAdministracion()
+        {
+            return DAL.ObtenerTodosAdministracion();
+        }
+        public int EliminarAdministracion(Int64 pId)
+        {
+            return DAL.EliminarAdministracion(pId);
+        }
+
+        public Administrador ObtenerAdministracionporId(Int64 pId)
+        {
+            return DAL.ObtenerAdministracionporId(pId);
+        }
+
+        public List<Administrador> ObtenerAdministracionporNombre(string pAdministracion)
+        {
+            return DAL.ObtenerAdministracionporNombre(pAdministracion);
+        }
+        
+        public List<Administrador> MostrarAdministracion()
+        {
+            return AdministradorDAL.MostrarAdministracion();
+        }
+        public List<Administrador> BuscarAdministracionPorNombre(string pNombre)
+        {
+            return AdministradorDAL.BuscarAdministracionPorNombre(pNombre);
         }
     }
 }
