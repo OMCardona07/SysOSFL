3c7897dd75ebb602957bc4fb4cb8e862f0d7f3b4
Josué Cardona <josuecardona07@hotmail.com>
2020-06-15 15:13:53 -0600

    Corrigiendo errores de interfaz

---
+++ SysOSFL.GUID/ControlProyectos.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SysOSFL.BL;
+using SysOSFL.EN;
+
+namespace SysOSFL.GUID
+{
+    
+    public partial class ControlProyectos : System.Web.UI.Page
+    {
+        ProyectosEN _Proyecto = new ProyectosEN();
+        ProyectosBL _ProyectoBL = new ProyectosBL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string script = "alert('Se ha registrado exitosamente')";
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "Exito", script, true);
+            if (txtNombre_pro.Text != "" && txtTipo.Text != "" && txtCodigo.Text != "" && txtProgreso.Text != "")
+            {
+                ProyectosEN Proyec = new ProyectosEN();
+                Proyec.NombreProyecto = txtNombre_pro.Text;
+                Proyec.TipoProyecto = txtTipo.Text;
+                Proyec.IdProyecto = txtCodigo.Text;
+                Proyec.ProgresoProyecto = txtProgreso.Text;
+
+                _ProyectoBL.AgregarPro(Proyec);
+
+                txtNombre_pro.Text = "";
+                txtTipo.Text = "";
+                txtCodigo.Text = "";
+                txtProgreso.Text = "";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string script = "alert('Se ha eliminado exitosamente')";
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "Exito", script, true);
+            if (txtCodigo.Text != "")
+            {
+                ProyectosEN Proyec = new ProyectosEN();
+
+                Proyec.IdProyecto = txtCodigo.Text;
+                _ProyectoBL.eliminarPro(Proyec);
+
+
+                txtCodigo.Text = "";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            string script = "alert('Lista de proyectos')";
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "Exito", script, true);
+
+            this.GridView1.DataSource = _ProyectoBL.ObtenerPro();
+            GridView1.DataBind();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            string script = "alert('Se ha modificado exitosamente')";
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "Exito", script, true);
+            if (txtNombre_pro.Text != "" && txtTipo.Text != "" && txtCodigo.Text != "" && txtProgreso.Text != "")
+            {
+                ProyectosEN Proyec = new ProyectosEN();
+                Proyec.NombreProyecto = txtNombre_pro.Text;
+                Proyec.TipoProyecto = txtTipo.Text;
+                Proyec.IdProyecto = txtCodigo.Text;
+                Proyec.ProgresoProyecto = txtProgreso.Text;
+
+                _ProyectoBL.ModificarPro(Proyec);
+
+
+
+                txtNombre_pro.Text = "";
+                txtTipo.Text = "";
+                txtCodigo.Text = "";
+                txtProgreso.Text = "";
+
+            }
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            string script = "alert('Se ha encontrado')";
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "Exito", script, true);
+            if (txtCodigo.Text != "")
+            {
+                ProyectosEN Proyec = new ProyectosEN();
+
+                Proyec.IdProyecto = txtCodigo.Text;
+
+
+
+                txtCodigo.Text = "";
+
+
+                this.GridView1.DataSource = _ProyectoBL.buscarProyec(Proyec); ;
+                GridView1.DataBind();
+            }
+    }
+}
---
+++ SysOSFL.GUID/ControlProyectos.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SysOSFL.GUID
+{
+
+
+    public partial class ControlProyectos
+    {
+
+        /// <summary>
+        /// Control txtNombre_pro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre_pro;
+
+        /// <summary>
+        /// Control txtTipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTipo;
+
+        /// <summary>
+        /// Control txtCodigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodigo;
+
+        /// <summary>
+        /// Control txtProgreso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtProgreso;
+
+        /// <summary>
+        /// Control btn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Control Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// Control Button4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
+
+        /// <summary>
+        /// Control Button5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button5;
+
+        /// <summary>
+        /// Control Button3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button3;
+
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+    }
+}
